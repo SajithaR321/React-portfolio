17fab49e29d44900bfedf441c4c7cbef3a410bfe
SajithaR321 <96288@fhss.sjp.ac.lk>
2022-03-01 00:37:52 +0530

    first commit

---
+++ src/yourdata.js
@@ -0,0 +1,184 @@
+// Skills Icons
+import htmlIcon from "./images/html.svg"
+import cssIcon from "./images/css.svg"
+import reactIcon from "./images/react.svg"
+import jsIcon from "./images/javascript.svg"
+import designIcon from "./images/design.svg"
+import codeIcon from "./images/code.svg"
+
+// Social Icon
+import githubIcon from "./images/github.svg"
+import instagramIcon from "./images/instagram.svg"
+
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+
+  //   Header Details ---------------------
+  name: "Sajitha",
+  headerTagline: [
+    //Line 1 For Header
+    "Devloper, UI designing",
+    //Line 2 For Header
+    "designing products, brands, illustrating",
+    //Line 3 For Header
+    "and experience",
+  ],
+  //   Header Paragraph
+  headerParagraph:
+    "Designing with Empathy for the human experience with Focusing on UX UI Design and Brand Identity ",
+
+  //Contact Email
+  contactEmail: "sajitharasanga123@gmail.com",
+
+  // End Header Details -----------------------
+
+  // Work Section ------------------------
+  projects: [
+    {
+      title: "Shoe Shop App", //Project Title - Add Your Project Title Here
+      para:
+        "A simple and attractive one-stop destination to shop for shoes.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://i.imgur.com/gDRsU6w.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/FHSS-USJ/final-project-SajithaR321-1.git",
+    },
+    {
+      title: "Jewellery Shop App", //Project Title - Add Your Project Title Here
+      para:
+        "An innovative online jewellery store app based in Sri Lanka, showcasing international styles of fashion jewellery and accessories.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://i.imgur.com/NGsD4Kz.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/FHSS-USJ/final-project-SajithaR321.git",
+    },
+    {
+      title: "School Management System App", //Project Title - Add Your Project Title Here
+      para:
+        "An information management system App for educational institutions to manage all student's data.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://i.imgur.com/dBoK1dD.png",
+      //Project URL - Add Your Project Url Here
+      url: "https://github.com/FHSS-USJ/final-project-SajithaR321.git",
+    },
+    {
+      title: "Coffee Table Book", //Project Title - Add Your Project Title Here
+      para:
+        "An appreciation for Fashion brands of a Music Group.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://i.imgur.com/3dUbyZ4.jpg",
+      //Project URL - Add Your Project Url Here
+      url: "",
+    },
+    {
+      title: "UI Design ", //Project Title - Add Your Project Title Here
+      para:
+        "User interface design for the Shoe Shop App.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://i.imgur.com/TiE4AA5.jpg",
+      //Project URL - Add Your Project Url Here
+      url: "",
+    },
+    {
+      title: "Illustrations", //Project Title - Add Your Project Title Here
+      para:
+        "My Illustrations such as posters, flyers, magazines, books.", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://i.imgur.com/X5BIUsd.jpg",
+      //Project URL - Add Your Project Url Here
+      url: "",
+    },
+
+    /*
+    If You Want To Add More Project just Copy and Paste This At The End
+    ,{
+        title: 'Project Five',
+        para: 'Something Amazing',
+        imageSrc: "",
+        url: ''
+    }
+    */
+  ],
+
+  // End Work Section -----------------------
+
+  // About Secton --------------
+  aboutParaOne:
+    "Hello!, I am Sajitha Rasanga.I'm From Sri Lanka",
+  aboutParaTwo:
+    "I am a web & soft­ware devel­op­er, aproduct deigner,experience designer,interaction,UI,UX, an artist and illustrator. Here you can see some of my latest works.",
+  aboutParaThree:
+    "My aundant energy fuels me in the pursuit of many interests,hobbies,areas of study and artistic endeavors.i'm a fas learner, able to pick up new skills and juggle different projects and roles with realative ease.I like to work expertise in a number of areas over the course of my life and career.My personality type INTP.",
+  aboutImage:
+    "https://i.imgur.com/ZGTEiW6.jpg",
+  //   End About Section ---------------------
+
+  // Skills Section ---------------
+
+  //   Import Icons from the top and link it here
+
+  skills: [
+    {
+      img: htmlIcon,
+      para:
+        "HTML",
+    },
+    {
+      img: cssIcon,
+      para:
+        "CSS",
+    },
+    {
+      img: jsIcon,
+      para:
+        "JavaScript",
+    },
+    {
+      img: reactIcon,
+      para:
+        "React",
+    },
+    {
+      img: designIcon,
+      para:
+        "Designing",
+    },
+    {
+      img: codeIcon,
+      para:
+        "Coding",
+    },
+  ],
+
+  // End Skills Section --------------------------
+
+  //   Promotion Section --------------------------
+
+  promotionHeading: "Let's make somethin' fun together!",
+  promotionPara:
+    "Feel free to snoop around on my social media.For inquiries or a how you doing contact me,I will be happy to hear from you..",
+  // End Promotion Section -----------------
+
+  //   Contact Section --------------
+
+ 
+  social: [
+    // Add Or Remove The Link Accordingly
+    { img: githubIcon, url: "https://github.com/SajithaR321" },
+    {
+      img: instagramIcon,
+      url: "https://www.instagram.com/_sajitha_rasanga_/",
+    },
+  ],
+
+  // End Contact Section ---------------
+}
+
+// Thanks for using this template, I would love to hear from you contact me at hello@chetanverma.com
